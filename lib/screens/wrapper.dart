0c3727cc2503434a3f7c724c567299a40dcac1b2
salimelewa <elewas@sheridancollege.ca>
2020-09-29 22:42:11 -0400

    Created Streamer to listen for user log in

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,15 @@
+import 'package:firecode_violation_detection/models/user.dart';
 import 'package:firecode_violation_detection/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return either the Home or Authenticate widget
+
+    final user = Provider.of<User>(context);
+    print(user);
     return Authenticate();
   }
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -5,8 +5,12 @@ class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   //create user obj based on FirebaseUser
-  User userFromFirebaseUser(FirebaseUser user) {
+  User _userFromFirebaseUser(FirebaseUser user) {
     return user != null ? User(uid: user.uid) : null;
+  }
+
+  Stream<User> get user {
+    return _auth.onAuthStateChanged.map(_userFromFirebaseUser);
   }
 
   //sign in anon
@@ -14,7 +18,7 @@ class AuthService {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return userFromFirebaseUser(user);
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
