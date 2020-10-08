e3c7b33572dcecb1710444cbe0386536d453d08b
salimelewa <elewas@sheridancollege.ca>
2020-10-08 01:03:19 -0400

    Addeed drop down option for floor amount.

--- lib/screens/home/add_building.dart
+++ lib/screens/home/add_building.dart
@@ -3,21 +3,45 @@ import 'package:flutter/material.dart';
 
 /// This is the main application widget.
 class AddBuilding extends StatelessWidget {
+  _AddBuildingFormState createState() => _AddBuildingFormState();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: MyStatelessWidget(),
+        body: AddBuildingForm(),
       ),
     );
   }
 }
 
 /// This is the stateless widget that the main application instantiates.
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({Key key}) : super(key: key);
+class AddBuildingForm extends StatefulWidget {
+  AddBuildingForm({Key key}) : super(key: key);
+
+  @override
+  _AddBuildingFormState createState() => _AddBuildingFormState();
+}
+
+class _AddBuildingFormState extends State<AddBuildingForm> {
   final _formKey = GlobalKey<FormState>();
+
+  final List<String> floors = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
+  ];
+
+  // ignore: unused_field
+  var _numFloors = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -31,12 +55,23 @@ class MyStatelessWidget extends StatelessWidget {
                   textInputDecoration.copyWith(labelText: 'Building Name'),
               onChanged: (val) {}),
           SizedBox(height: 10.0),
-          TextFormField(
-              validator: (val) =>
-                  val.isEmpty ? 'Enter a how many floors.' : null,
-              decoration:
-                  textInputDecoration.copyWith(labelText: 'Number of floors'),
-              onChanged: (val) {}),
+          DropdownButtonFormField(
+            decoration: textInputDecoration,
+            value: '1',
+            items: floors.map((floors) {
+              return DropdownMenuItem(
+                value: floors,
+                child: Text('$floors'),
+              );
+            }).toList(),
+            onChanged: (val) => setState(() => _numFloors = val),
+          ),
+          // TextFormField(
+          //     validator: (val) =>
+          //         val.isEmpty ? 'Enter a how many floors.' : null,
+          //     decoration:
+          //         textInputDecoration.copyWith(labelText: 'Number of floors'),
+          //     onChanged: (val) {}),
           SizedBox(height: 10.0),
           TextFormField(
               validator: (val) =>
